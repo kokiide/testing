b52a91ac6750fe940a15e9e9a5d01b6533135d67
Keio_SFC <keio_sfc@net76-dhcp133.sfc.keio.ac.jp>
2017-05-25 20:40:33 +0900

    1st

--- CollectionViewTrial2/ViewController.swift
+++ CollectionViewTrial2/ViewController.swift
@@ -8,16 +8,93 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SettingTopViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    var collectionView: UICollectionView!
+    
+    let items = [
+    "月曜日",
+    "火曜日",
+    "水曜日",
+    "木曜日",
+    "金曜日",
+    "土曜日",
+    "日曜日",
+    ]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.registerClass(CustomUICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = UIColor.clearColor()
+        
+        self.view.addSubview(collectionView)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell: CustomUICollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell",
+                                                                                                     forIndexPath: indexPath) as! CustomUICollectionViewCell
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    // ⭐️これがポイントかも
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let width: CGFloat = self.view.frame.width / 3 - 2
+        let height: CGFloat = width
+        return CGSize(width: width, height: height) // The size of one cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        let frame: CGRect = self.view.frame
+        let margin: CGFloat = 0
+        return UIEdgeInsetsMake(margin, margin, margin, margin) // margin between cells
+    }
+    
+}
+
+class CustomUICollectionViewCell : UICollectionViewCell{
+    var textLabel : UILabel?
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        textLabel = UILabel(frame: CGRectMake(0, 0, frame.width, frame.height))
+        textLabel?.text = ""
+        textLabel?.font = UIFont.systemFontOfSize(12)
+        textLabel?.backgroundColor = UIColor.whiteColor()
+        textLabel?.textAlignment = NSTextAlignment.Center
+        self.contentView.addSubview(textLabel!)
     }
 
 
