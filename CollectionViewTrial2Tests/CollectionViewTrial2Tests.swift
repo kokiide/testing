120d7abfbb077306a7ee348bb436664053751e2b
Keio_SFC <keio_sfc@net76-dhcp133.sfc.keio.ac.jp>
2017-05-25 20:30:03 +0900

    Initial Commit

---
+++ CollectionViewTrial2Tests/CollectionViewTrial2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CollectionViewTrial2Tests.swift
+//  CollectionViewTrial2Tests
+//
+//  Created by Keio_SFC on 2017/05/25.
+//  Copyright © 2017年 kokiide. All rights reserved.
+//
+
+import XCTest
+@testable import CollectionViewTrial2
+
+class CollectionViewTrial2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
